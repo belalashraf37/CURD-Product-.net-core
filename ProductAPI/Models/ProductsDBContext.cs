8f65fb71b919226a5625942b8d0b7b90022a5611
Belal Ashraf <48225483+belalashraf37@users.noreply.github.com>
2021-07-25 22:29:07 +0200

    first commit

---
+++ ProductAPI/Models/ProductsDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ProductAPI.Models
+{
+    public partial class ProductsDBContext : DbContext
+    {
+       
+
+        public ProductsDBContext(DbContextOptions<ProductsDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+       
+    }
+}
